6ef749b863f617b32ea4d99d3512454082ac5caf
godievda-pixel <godievda@gmail.com>
2026-09-14 12:39:12 +0300

    feat: wire QPF read APIs into worker entrypoint

--- finance-worker/src/main.js
+++ finance-worker/src/main.js
@@ -1,10 +1,18 @@
 import { validateTelegramInitData } from './telegram.js';
-import { findActiveAdminByTelegramId } from './supabase.js';
+import {
+  findActiveAdminByTelegramId,
+  getDashboardSummary,
+  listEmployeesWithBalances,
+  listEventsWithFinancials,
+} from './supabase.js';
 import { createWorker } from './worker.js';
 
 const app = createWorker({
   validateInitData: validateTelegramInitData,
-  findAdmin: findActiveAdminByTelegramId,
+  findAdmin: (telegramId, env) => findActiveAdminByTelegramId(telegramId, env),
+  getDashboard: (query, env) => getDashboardSummary(query, env),
+  listEmployees: (env) => listEmployeesWithBalances(env),
+  listEvents: (query, env) => listEventsWithFinancials(query, env),
 });
 
 export default app;
